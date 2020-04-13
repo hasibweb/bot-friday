a466f5034c5663479a10e1cae132bbbddb9d3e2b
hasibweb <hasibonline2@gmail.com>
2020-04-13 22:02:08 +0600

    port

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ app.get("/", (req, res) => {
   res.send("I am Alive..");
 });
 
-const PORT = (process.env.PORT = 5000);
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("App Running on Port:", PORT));
