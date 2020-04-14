e28e3e2d30a5e244096f394097d21563e2b25855
hasibweb <hasibonline2@gmail.com>
2020-04-15 02:52:57 +0600

    net

--- bot.js
+++ bot.js
@@ -9,13 +9,15 @@ manager.addDocument("en", "bye for now", "greetings.bye");
 manager.addDocument("en", "i must go", "greetings.bye");
 manager.addDocument("en", "hello", "greetings.hello");
 manager.addDocument("en", "hi", "greetings.hello");
-manager.addDocument("en", "howdy", "greetings.hello");
+manager.addDocument("en", "hey", "greetings.hello");
 
 // Train also the NLG
 manager.addAnswer("en", "greetings.bye", "Till next time");
 manager.addAnswer("en", "greetings.bye", "see you soon!");
 manager.addAnswer("en", "greetings.hello", "Hey there!");
-manager.addAnswer("en", "greetings.hello", "Greetings!");
+manager.addAnswer("en", "greetings.hello", "Hi, How are you?");
+manager.addAnswer("en", "greetings.hello", "Hello sir!");
+manager.addAnswer("en", "greetings.hello", "hey, what's up");
 
 // Train and save the model.
 (async () => {
